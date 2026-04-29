fa6e2b2283696ab18e028720edf8420ca7c80948
Chhoung Bunchhean <bchhean8@gmail.com>
2026-04-29 11:34:26 +0700

    Updated

--- src/main/java/com/taskflow/studentmanagement/repository/DepartmentRepository.java
+++ src/main/java/com/taskflow/studentmanagement/repository/DepartmentRepository.java
@@ -1,6 +1,7 @@
 package com.taskflow.studentmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.taskflow.studentmanagement.entity.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
